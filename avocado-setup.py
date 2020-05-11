lop-devops/tests

commit 4711ef2a787bbd043353561fdd7244e72bac14ee

Displaying result status in summary
When multiple tests are run, along with kernel messages output,
it becomes difficult to read the status of the tests. This commits
tries to solve that by displaying the status in the summary.

Signed-off-by: Narasimhan V <sim@linux.vnet.ibm.com>

avocado-setup.py
@@ -18,6 +18,7 @@
 import os
 import shutil
 import time
+import json
 import sys
 import shlex
 import argparse
@@ -377,7 +378,13 @@ def run_test(testsuite, avocado_bin):
     logger.info('')
     result_link = testsuite.jobdir()
     if result_link:
-        result_link += "/job.log"
+        result_json = result_link + "/results.json"
+        result_link += "/job.log\n"
+        with open(result_json, encoding = "utf-8") as fp:
+            result_state = json.load(fp)
+        for state in ['pass', 'cancel', 'errors', 'failures', 'skip', 'warn', 'interrupt']:
+            if state in result_state.keys():
+                result_link += "| %s %s |" % (state.upper(), str(result_state[state]))
         testsuite.runstatus("Run", "Successfully executed", result_link)
     else:
         testsuite.runstatus("Not_Run", "Unable to find job log file")

commit 4a04de56f6cf836fcebe4a59bea697ab84f3d273

Fix pep8 issues to fix Travis failure
Looks some of past commits like 393e566a, 4711ef2a
introduced bunch of pep8 issues and I have overlooked and
force merged them, which makes travis fail all Pull Request.
Let's fixup the same.

Signed-off-by: Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>

README
@@ -322,7 +322,7 @@
     OpenPOWER Host OS repositories on GitHub:
       https://github.com/open-power-host-os
 
-8. Pre/Post Script support 
+8. Pre/Post Script support
 
   User need to popluate their pre/post scripts as  prescript and postscript directory
   * Create a directory named "prescript" inside config/ directory  and populated with pre scripts

avocado-setup.py
@@ -31,8 +31,8 @@ from lib import helper
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH = "%s/config/wrapper/env.conf" % BASE_PATH
-prescript="%s/config/prescript" % BASE_PATH
-postscipt="%s/config/postscript" % BASE_PATH
+prescript = "%s/config/prescript" % BASE_PATH
+postscipt = "%s/config/postscript" % BASE_PATH
 NORUNTEST_PATH = "%s/config/wrapper/no_run_tests.conf" % BASE_PATH
 TEST_CONF_PATH = "%s/config/tests/" % BASE_PATH
 CONFIGFILE = configparser.SafeConfigParser()
@@ -48,8 +48,8 @@ TEST_DIR = "%s/tests" % BASE_PATH
 DATA_DIR = "%s/data" % BASE_PATH
 LOG_DIR = "%s/results" % BASE_PATH
 logger = logger_init(filepath=BASE_PATH).getlogger()
-prescript_dir=CONFIGFILE.get('script-dir', 'prescriptdir')
-postscipt_dir=CONFIGFILE.get('script-dir', 'postscriptdir')
+prescript_dir = CONFIGFILE.get('script-dir', 'prescriptdir')
+postscipt_dir = CONFIGFILE.get('script-dir', 'postscriptdir')
 
 
 class TestSuite():
@@ -271,7 +271,6 @@ def install_optional_plugin(plugin):
         pass
 
 
-
 def create_config(logdir):
     """
     Create the local avocado config file
@@ -350,6 +349,7 @@ def bootstrap(enable_kvm=False):
             os.makedirs(postscipt_dir)
         helper.copy_dir_file(postscipt, postscipt_dir)
 
+
 def run_test(testsuite, avocado_bin):
     """
     To run given testsuite
@@ -392,7 +392,7 @@ def run_test(testsuite, avocado_bin):
     if result_link:
         result_json = result_link + "/results.json"
         result_link += "/job.log\n"
-        with open(result_json, encoding = "utf-8") as fp:
+        with open(result_json, encoding="utf-8") as fp:
             result_state = json.load(fp)
         for state in ['pass', 'cancel', 'errors', 'failures', 'skip', 'warn', 'interrupt']:
             if state in result_state.keys():
@@ -417,6 +417,7 @@ def env_clean():
 
     if os.path.isdir(postscipt_dir):
         helper.remove_file(postscipt, postscipt_dir)
+
 
 def edit_mux_file(test_config_name, mux_file_path, tmp_mux_path):
     """